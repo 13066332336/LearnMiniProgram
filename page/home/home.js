ede326e4aa420ec28a1799927f48db5ae926e7df
13066332336 <1358849720@qq.com>
2019-06-26 17:37:39 +0800

    注册小程序和页面

--- page/home/home.js
+++ page/home/home.js
@@ -1,66 +1,38 @@
 // page/home/home.js
+
+//获取全局app的数据
+const app = getApp();
+const name = app.globalDate.name;
+const age = app.globalDate.age;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    name:'yangclenglong',
+    age:13,
+    students: [
+      { name: 'q', age: 12, num: 99, class: '中二' },
+      { name: 'a', age: 11, num: 97, class: '中二' },
+      { name: 'w', age: 13, num: 93, class: '中三' },
+      { name: 'm', age: 14, num: 96, class: '中一' }
+    ],
+    counter:0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+     this.setData({
+       counter:this.data.counter +=1
+     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  jian(){
+    this.setData({
+      counter:this.data.counter -=1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //用户授权
+  handleGetUserInfo(event){
+    console.log(event)
   }
 })
